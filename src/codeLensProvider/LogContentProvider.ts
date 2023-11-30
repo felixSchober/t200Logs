9072621f4acc0fe2a0c94faca77db6f1c0ca4107
Felix Schober <feschobe@microsoft.com>
2023-11-30 15:27:32 +0000

    webview panel

---
+++ src/codeLensProvider/LogContentProvider.ts
@@ -0,0 +1,292 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
+import * as fs from "fs/promises";
+
+import * as vscode from "vscode";
+
+/**
+ * The event that is fired when the filter changes.
+ */
+export type FilterChangedEvent = {
+    /**
+     * The time filter.
+     */
+    timeFilter: string | null;
+    /**
+     * The keyword filter.
+     */
+    keywordFilter: string | null;
+};
+
+type LogEntry = {
+    /**
+     * The date of the log entry.
+     */
+    date: Date;
+    /**
+     * The text of the log entry.
+     */
+    text: string;
+
+    /**
+     * The service that generated the log entry.
+     */
+    service?: string;
+};
+
+type ServiceFiles = {
+    /**
+     * The name of the service.
+     */
+    serviceName: string;
+    /**
+     * The files of the service.
+     */
+    files: vscode.Uri[];
+};
+
+const MAX_LOG_FILES_PER_SERVICE = 2;
+
+/**
+ * A content provider that transforms the content of a log file.
+ */
+export class LogContentProvider implements vscode.TextDocumentContentProvider {
+    // private readonly isDateRegexForHiding = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}\+\d{2}:\d{2}/;
+    // private readonly webDateRegexForHiding = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/;
+
+    private timeFilter: string | null = null;
+    private keywordFilter: string | null = null;
+
+    public static readonly documentScheme = "log-viewer";
+
+    public static readonly documentUri = vscode.Uri.parse(`${this.documentScheme}:/log-viewer.log`);
+
+    /**
+     * Strings to remove from the log entries.
+     */
+    private readonly stringsToRemove = [
+        /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}\+\d{2}:\d{2}/,
+        /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/,
+        /\sInf\s/,
+        /-logs\.txt/,
+    ];
+
+    private readonly stringReplacementMap = [
+        {
+            searchString: "AuthenticationService: [Auth]",
+            replacementString: "[Auth]",
+        },
+        {
+            searchString: "CDLWorkerCacheManager: [CDLWorkerCacheManager]",
+            replacementString: "[CDLWorkerCacheManager]",
+        },
+    ];
+
+    private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+    readonly onDidChange = this._onDidChange.event;
+
+    private changeTrigger = 0;
+
+    /**
+     * Creates a new instance of the LogContentProvider class.
+     * @param onFilterChangeEvent The event that is fired when the filter changes.
+     */
+    constructor(onFilterChangeEvent: vscode.Event<FilterChangedEvent>) {
+        onFilterChangeEvent(filterChangeEvent => {
+            this.updateFilters(filterChangeEvent.timeFilter, filterChangeEvent.keywordFilter);
+        });
+    }
+
+    /**
+     * Updates the filters for the log entries.
+     * @param timeFilter The time filter.
+     * @param keywordFilter The keyword filter.
+     */
+    private updateFilters(timeFilter: string | null, keywordFilter: string | null) {
+        this.timeFilter = timeFilter;
+        this.keywordFilter = keywordFilter;
+
+        this.changeTrigger++;
+        this._onDidChange.fire(LogContentProvider.documentUri);
+    }
+
+    /**
+     * Provide textual content for a given uri.
+     * @param uri An uri which scheme matches the scheme this provider was created for.
+     * @returns A string representing the textual content.
+     */
+    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+        if (!vscode.workspace.workspaceFolders) {
+            return "";
+        }
+
+        console.log("Content requested for changeTrigger:", this.changeTrigger);
+
+        const logFiles = await vscode.workspace.findFiles("**/*.{log,txt}", "**/node_modules/**", 100);
+
+        // Group files by service and sort them by date
+        const serviceFiles = this.groupAndSortFiles(logFiles);
+
+        // Read and parse all log files
+        let logEntries: LogEntry[] = [];
+        for (const filesForService of serviceFiles) {
+            // Get the most recent two files for each service
+            const recentFiles = filesForService.files.slice(0, MAX_LOG_FILES_PER_SERVICE);
+            for (const file of recentFiles) {
+                const content = await fs.readFile(file.fsPath, "utf8");
+                logEntries = logEntries.concat(this.parseLogContent(content, filesForService.serviceName));
+            }
+        }
+
+        // Sort log entries by date
+        logEntries.sort((a, b) => a.date.getTime() - b.date.getTime());
+
+        // Generate content for the virtual document
+        return this.generateDocumentContent(logEntries);
+    }
+
+    /**
+     * Groups the given files by service and sorts them by date.
+     * @param files The files to group and sort.
+     * @returns A record of service names and their files.
+     */
+    groupAndSortFiles(files: vscode.Uri[]): ServiceFiles[] {
+        const fileGroups: Record<string, ServiceFiles> = {};
+
+        for (const file of files) {
+            const filename = file.path.split("/").pop();
+            if (filename) {
+                const parts = filename.split("_");
+                const serviceName = parts[0];
+
+                if (fileGroups[serviceName]) {
+                    fileGroups[serviceName].files.push(file);
+                    continue;
+                }
+
+                fileGroups[serviceName] = {
+                    serviceName,
+                    files: [file],
+                };
+            }
+        }
+
+        // Sort files within each group by date
+        const result: ServiceFiles[] = [];
+        for (const serviceName in fileGroups) {
+            // only sort files if there are more than 2 files
+            if (fileGroups[serviceName].files.length < MAX_LOG_FILES_PER_SERVICE) {
+                fileGroups[serviceName].files.sort((a, b) => {
+                    const aTimestamp = this.extractTimestamp(a.path);
+                    const bTimestamp = this.extractTimestamp(b.path);
+                    return bTimestamp - aTimestamp; // Sort in descending order
+                });
+            }
+            result.push(fileGroups[serviceName]);
+        }
+
+        return result;
+    }
+
+    /**
+     * Extracts the timestamp from the given file path.
+     * Example: MSTeams_2023-11-23_12-40-44.33.log.
+     * @param filePath The file path to extract the timestamp from.
+     * @returns The timestamp of the file.
+     */
+    extractTimestamp(filePath: string): number {
+        const regex = /_(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})/;
+        const match = filePath.match(regex);
+        return match ? new Date(match[1].replace(/-/g, "/").replace("_", " ")).getTime() : 0;
+    }
+
+    /**
+     * Parses the content of a log file and returns log entries with their dates.
+     * @param content The content of the log file.
+     * @param serviceName The name of the service that generated the log file.
+     * @returns An array of log entries with their dates.
+     */
+    parseLogContent(content: string, serviceName: string): Array<LogEntry> {
+        // matches 2023-11-28T15:16:31.758465+00:00
+        const isoDateRegex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}\+\d{2}:\d{2}/;
+
+        // matches 2023-11-29T10:21:49.895Z
+        const webDateRegex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/;
+
+        this.stringReplacementMap.forEach(replacement => {
+            content = content.replaceAll(replacement.searchString, replacement.replacementString);
+        });
+
+        return content
+            .split("\n")
+            .filter(entry => this.matchesFilter(entry))
+            .map(line => {
+                const match = line.match(isoDateRegex) || line.match(webDateRegex);
+                return {
+                    date: match ? new Date(match[0]) : new Date(0), // Default to epoch if no date found
+                    text: line,
+                    service: serviceName,
+                };
+            });
+    }
+
+    /**
+     * Checks if the given log entry should be filtered out.
+     * @param logEntryLine The log entry to check.
+     * @returns True if the log entry should stay, false if it should be filtered out.
+     */
+    private matchesFilter(logEntryLine: string): boolean {
+        if (this.timeFilter) {
+            const timeRegex = new RegExp(this.timeFilter);
+            const timeMatch = logEntryLine.match(timeRegex);
+            if (!timeMatch) {
+                return false;
+            }
+        }
+
+        if (this.keywordFilter) {
+            const keywordRegex = new RegExp(this.keywordFilter);
+            const keywordMatch = logEntryLine.match(keywordRegex);
+            if (!keywordMatch) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Generates the content for the virtual document.
+     * @param logEntries The log entries to include in the document.
+     * @returns The content of the virtual document.
+     */
+    generateDocumentContent(logEntries: Array<LogEntry>): string {
+        let currentSecond: Date | null = null;
+        let documentContent = "";
+
+        for (const entry of logEntries) {
+            // Check if the entry is in a new second
+            if (!currentSecond || entry.date.getSeconds() !== currentSecond.getSeconds()) {
+                if (currentSecond !== null) {
+                    // Add a foldable region marker (this is a placeholder, actual folding is handled elsewhere)
+                    documentContent += "// Folding region end\n\n";
+                }
+                currentSecond = entry.date;
+                documentContent += `// ${currentSecond.toISOString()}\n`;
+            }
+
+            // removes all information that is not needed one by one
+            const entryText = this.stringsToRemove.reduce((text, regex) => text.replace(regex, ""), entry.text);
+            documentContent += `${entry.service} ${entryText}\n`;
+        }
+
+        // replace the hex values at the beginning of each log line
+        const hexRegex = /\s0x[0-9a-fA-F]{16}\s/g;
+        documentContent = documentContent.replace(hexRegex, "");
+
+        return documentContent;
+    }
+}
+
--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,11 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  */
 
+/* eslint-disable max-classes-per-file */
 import * as vscode from "vscode";
+
+import { GoToSourceLensProvider } from "./codeLensProvider/GoToSourceLensProvider";
+import { FilterChangedEvent, LogContentProvider } from "./codeLensProvider/LogContentProvider";
 
 /**
  * Activate the extension.
@@ -12,17 +16,74 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "t200logs" is now active!');
+    const onFilterChanged = new vscode.EventEmitter<FilterChangedEvent>();
+    const provider = new LogContentProvider(onFilterChanged.event);
+    context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(LogContentProvider.documentScheme, provider));
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand("t200logs.parseLogs", () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        void vscode.window.showInformationMessage("Hello World from T200Logs!");
+    let codeLensDisposable = vscode.languages.registerCodeLensProvider(
+        { scheme: "file", pattern: "**/desktop/**" },
+        new GoToSourceLensProvider()
+    );
+
+    let disposableDecoration = vscode.commands.registerCommand("t200logs.hideIsoDates", () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const text = editor.document.getText();
+            const isoDateRegex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}\+\d{2}:\d{2}/g;
+            const decorationsArray: vscode.DecorationOptions[] = [];
+
+            let match;
+            while ((match = isoDateRegex.exec(text))) {
+                const date = new Date(match[0]);
+                const humanReadableDate = date.toLocaleString(); // Convert to a human-readable format
+
+                const startPos = editor.document.positionAt(match.index);
+                const endPos = editor.document.positionAt(match.index + match[0].length);
+
+                const decoration = {
+                    range: new vscode.Range(startPos, endPos),
+                    renderOptions: {
+                        after: {
+                            contentText: ` [${humanReadableDate}]`,
+                            color: "lightgrey", // You can adjust the color
+                            fontWeight: "bold",
+                            textDecoration: "none;",
+                        },
+                    },
+                };
+
+                decorationsArray.push(decoration);
+            }
+
+            const decorationType = vscode.window.createTextEditorDecorationType({});
+            // editor.setDecorations(decorationType, decorationsArray);
+        }
     });
 
-    context.subscriptions.push(disposable);
+    console.log("T200Logs extension activated");
+
+    const foldingProvider = new LogFoldingRangeProvider();
+    context.subscriptions.push(
+        vscode.languages.registerFoldingRangeProvider(
+            { scheme: "file", language: "log" }, // Adjust the language if necessary
+            foldingProvider
+        )
+    );
+
+    context.subscriptions.push(codeLensDisposable);
+    context.subscriptions.push(disposableDecoration);
+
+    // Add a command to open the virtual document
+    let openLogViewerDisposable = vscode.commands.registerCommand("t200logs.openLogViewer", async () => {
+        const doc = await vscode.workspace.openTextDocument(LogContentProvider.documentUri);
+        await vscode.window.showTextDocument(doc, { preview: false });
+    });
+
+    context.subscriptions.push(openLogViewerDisposable);
+
+    const panelDisposable = vscode.window.registerWebviewViewProvider("t200logs", new LogsWebviewViewProvider(onFilterChanged));
+    context.subscriptions.push(panelDisposable);
+
 }
 
 /**
@@ -30,3 +91,193 @@ export function activate(context: vscode.ExtensionContext) {
  */
 export function deactivate() {}
 
+/**
+ * A folding range provider that folds all lines that start with an ISO date.
+ */
+class LogFoldingRangeProvider implements vscode.FoldingRangeProvider {
+    private decorationType: vscode.TextEditorDecorationType;
+
+    /**
+     * Creates a new instance of the folding range provider.
+     */
+    constructor() {
+        this.decorationType = vscode.window.createTextEditorDecorationType({
+            color: "transparent", // or the background color to 'hide' the text
+        });
+    }
+
+    /**
+     * Provide folding ranges for the given document.
+     * @param document The document for which the folding ranges should be computed.
+     * @returns An array of folding ranges or `undefined` if the provider does not want to participate or was cancelled.
+     */
+    provideFoldingRanges(document: vscode.TextDocument): vscode.FoldingRange[] {
+        const foldingRanges: vscode.FoldingRange[] = [];
+        const isoDateRegex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/;
+        const isDateRegexForHiding = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}\+\d{2}:\d{2}/;
+        const decorations: vscode.DecorationOptions[] = [];
+
+        let currentSecond = null;
+        let startLine = null;
+
+        for (let i = 0; i < document.lineCount; i++) {
+            const lineText = document.lineAt(i).text;
+            const match = lineText.match(isoDateRegex);
+
+            if (match) {
+                const second = match[0];
+
+                if (currentSecond === second && startLine === null) {
+                    startLine = i;
+                } else if (currentSecond !== second) {
+                    if (startLine !== null) {
+                        foldingRanges.push(new vscode.FoldingRange(startLine, i - 1));
+
+                        // Apply decoration to hide dates except the first one in the region
+                        for (let line = startLine + 1; line <= i - 1; line++) {
+                            // get a new match to get the full date string
+                            const dateMatch = document.lineAt(line).text.match(isDateRegexForHiding);
+                            if (!dateMatch) {
+                                continue;
+                            }
+
+                            const startPos = document.lineAt(line).range.start;
+                            const endPos = startPos.translate(0, dateMatch[0].length);
+                            decorations.push({ range: new vscode.Range(startPos, endPos) });
+                        }
+                    }
+                    currentSecond = second;
+                    startLine = null;
+                }
+            }
+        }
+
+        // Set the decorations
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor) {
+            activeEditor.setDecorations(this.decorationType, decorations);
+        }
+
+        return foldingRanges;
+    }
+
+}
+
+/**
+ * The view provider for the side bar.
+ */
+class LogsWebviewViewProvider implements vscode.WebviewViewProvider {
+
+    /**
+     * Creates a new instance of the view provider.
+     * @param onFilterChanged The event emitter for when the filter changes.
+     */
+    constructor(private readonly onFilterChanged: vscode.EventEmitter<FilterChangedEvent>) { }
+
+    /**
+     * Resolves and defines a webview view.
+     * @param webviewView The webview we've been asked to resolve.
+     * @param context The context for the webview.
+     * @param token A cancellation token.
+     */
+    public resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext<unknown>, token: vscode.CancellationToken): void | Thenable<void> {
+        webviewView.webview.options = {
+            enableScripts: true
+        };
+
+        webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+
+        webviewView.webview.onDidReceiveMessage(
+            message => {
+                switch (message.command) {
+                    case "applyFilters":
+                        console.log(message);
+                        this.onFilterChanged.fire({
+                            timeFilter: message.timeFilter,
+                            keywordFilter: message.keywordFilter
+                        });
+                        break;
+                }
+            },
+            undefined
+        );
+    }
+
+    /**
+     * Returns the HTML content for the webview.
+     * @param webview The webview for which to return the HTML content.
+     * @returns The HTML content for the webview.
+     */
+    private getHtmlForWebview(webview: vscode.Webview): string {
+        // Use a nonce to whitelist which scripts can be run for security reasons
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src 'nonce-${nonce}'; style-src ${webview.cspSource};">
+            <title>Log Filter</title>
+            <style>
+                /* Add your styles here */
+                body {
+                    padding: 10px;
+                    font-family: Arial, sans-serif;
+                }
+                input, button {
+                    margin: 5px;
+                }
+            </style>
+        </head>
+        <body>
+            <div>
+                <label for="timeFilter">Time Filter:</label>
+                <input type="text" id="timeFilter" placeholder="e.g., 12:00-13:00">
+            </div>
+            <div>
+                <label for="keywordFilter">Keyword Filter:</label>
+                <input type="text" id="keywordFilter" placeholder="Enter keyword">
+            </div>
+            <button id="applyFilters">Apply Filters</button>
+
+            <script nonce="${nonce}">
+                const vscode = acquireVsCodeApi();
+
+                document.getElementById('applyFilters').addEventListener('click', () => {
+                    const timeFilter = document.getElementById('timeFilter').value;
+                    const keywordFilter = document.getElementById('keywordFilter').value;
+                    vscode.postMessage({
+                        command: 'applyFilters',
+                        timeFilter: timeFilter,
+                        keywordFilter: keywordFilter
+                    });
+                });
+            </script>
+        </body>
+        </html>`;
+    }
+
+
+
+}
+
+/**
+ * Returns a nonce for the webview.
+ * @returns A nonce for the webview.
+ */
+function getNonce() {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+
+
+
+
+
+
+
+}
