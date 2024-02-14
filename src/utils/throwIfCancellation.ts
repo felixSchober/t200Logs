ef31fed7937a926a5d832c2b52927cdd91c24679
Felix Schober <github@felix-schober.de>
2024-02-14 18:41:11 +0000

    add KeywordHighlightDecorator

--- src/utils/throwIfCancellation.ts
+++ src/utils/throwIfCancellation.ts
@@ -8,9 +8,10 @@ import { CancellationError, CancellationToken } from "vscode";
  * Throws an error if the given token is cancelled.
  * @param token The cancellation token.
  */
-export const throwIfCancellation = (token: CancellationToken) => {
-    if (token.isCancellationRequested) {
+export const throwIfCancellation = (token?: CancellationToken) => {
+    if (token?.isCancellationRequested) {
         console.log("Cancellation requested");
         throw new CancellationError();
     }
 };
+
