566124a505c82cfd589d5e806a473140c9e32ee9
Felix Schober <github@felix-schober.de>
2024-03-12 16:49:58 +0100

    feat: improve createRandomColor to not create dark colors

--- src/ui/src/common/createRandomColor.ts
+++ src/ui/src/common/createRandomColor.ts
@@ -3,13 +3,30 @@
  */
 
 /**
- * Create a random color in hex format.
- * @returns A random color in hex format.
- * @example
- * ```
- * const color = createRandomColor(); // #ff00ff
- * ```
+ * Generates a random hex color string.
+ * If `preventNearBlack` is true, the function ensures the color is not very dark.
+ *
+ * @param preventNearBlack Optional. If set, prevents the generation of near-black colors.
+ * @returns The generated hex color string.
  */
-export const createRandomColor = (): string => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+export function createRandomColor(preventNearBlack?: boolean): string {
+    let color = "#";
+    for (let i = 0; i < 3; i++) {
+      const value = Math.floor(Math.random() * 256);
+      color += ("0" + value.toString(16)).substr(-2);
+    }
+  
+    // If preventNearBlack is true, check if the generated color is too dark and regenerate if necessary.
+    // This is a simple heuristic that checks if the hexadecimal color, when converted to an integer,
+    // is below a certain threshold. Here, we're using 40 (hex: 28) for each RGB component as a threshold,
+    // meaning if the color is darker than #282828, we consider it "very dark".
+    if (preventNearBlack) {
+      const isDark = parseInt(color.slice(1), 16) < parseInt("282828", 16);
+      if (isDark) {
+        return createRandomColor(preventNearBlack);
+      }
+    }
+  
+    return color;
+  }
+  
